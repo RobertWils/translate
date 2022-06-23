2ffcb648af44f7f39002144874b09b49993f1734
RobertWils <robert.wils@singularitydao.ai>
2022-06-23 17:22:40 +0200

    first commit

---
+++ src/components/Button.js
@@ -0,0 +1,29 @@
+import React from "react";
+import LanguageContext from "../contexts/LanguageContext";
+import ColorContext from "../contexts/ColorContext";
+
+class Button extends React.Component {
+  renderText(value) {
+    return value === "english" ? "Submit" : "Verzenden";
+  }
+
+  renderButton(color) {
+    return (
+      <button className={`ui button ${color}`}>
+        <LanguageContext.Consumer>
+          {(value) => this.renderText(value)}
+        </LanguageContext.Consumer>
+      </button>
+    );
+  }
+
+  render() {
+    return (
+      <ColorContext.Consumer>
+        {(color) => this.renderButton(color)}
+      </ColorContext.Consumer>
+    );
+  }
+}
+
+export default Button;
